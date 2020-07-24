9b3ecbeb304ce54ba777213ca25320bd03ffbf8d
vandie <vandie@users.noreply.github.com>
2020-07-24 08:19:06 +0000

    deployed via Github Pages Deploy Action 🚀 for 8e63fff7e99ce05e19bb316bd2f719e6108e04af

---
+++ precache-manifest.91fc836486e7f1b4f1bd122b76f250dd.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb3787c795357167cc50",
+    "url": "/IMDBSearch/css/app.0cc6c62c.css"
+  },
+  {
+    "revision": "a0aae03d7b3633889243",
+    "url": "/IMDBSearch/css/chunk-5f664a34.8dab9547.css"
+  },
+  {
+    "revision": "fe649f114a37276337a0",
+    "url": "/IMDBSearch/css/chunk-80bb9c80.e6f97bb5.css"
+  },
+  {
+    "revision": "880328e1510c3e58d9bfb753103ce52a",
+    "url": "/IMDBSearch/index.html"
+  },
+  {
+    "revision": "cb3787c795357167cc50",
+    "url": "/IMDBSearch/js/app.10b03fc8.js"
+  },
+  {
+    "revision": "a0aae03d7b3633889243",
+    "url": "/IMDBSearch/js/chunk-5f664a34.9be14a77.js"
+  },
+  {
+    "revision": "fe649f114a37276337a0",
+    "url": "/IMDBSearch/js/chunk-80bb9c80.be73caaf.js"
+  },
+  {
+    "revision": "5488c281fd53becb4b18",
+    "url": "/IMDBSearch/js/chunk-vendors.4900b864.js"
+  },
+  {
+    "revision": "ebb58bb148b788c18e80dc3bb33cbd76",
+    "url": "/IMDBSearch/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/IMDBSearch/robots.txt"
+  }
+]);
